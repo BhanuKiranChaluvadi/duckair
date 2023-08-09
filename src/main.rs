a78233972c0bed13b838fa0dce0ebd0f78f9a3f5
Bhanu Kiran Chaluvadi <bkch@universal-robots.com>
2023-08-09 20:46:38 +0200

    variable samples

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,24 @@
 ]
 fn main() {
     println!("Hello, world!");
-    // let location: [f32; 2] = [0.0, 0.0];
-    // let location: [f32; 1000] = [0.0; 1000];
-    // let location: [f64; 2] = [41.4094069, -81.8546911];
-    let location:(&str, f64, f64) = ("KCLE", 41.4094069, -81.8546911);
-    let (name, latitude, longitude) = location;
-    println!("{} is at {}, {}", location.0, location.1, location.2);
-    println!("{} is at {}, {}", name, latitude, longitude);
-    // println!("{} is at {}, {}", location.name, location.latitude, location.longitude);
+    let x = 5.0;
+    let y: u16 = 5;
 
-    let person_name_slice = "Peter Parker";
-    let person_name_string = person_name_slice.to_string();
-    // let person_name_string_literal = "Peter Parker".to_string();
-    let person_name_string_literal = String::from("Peter Parker");
-    let person_name_to_slice = &person_name_string;
-    let person_name_slice2 = person_name_string.as_str();
+    let float_thirty_two: f32 = 17.2;
+    let unsigned_eight: u8 = 5;
+    let result = float_thirty_two  / unsigned_eight as f32;
 
-    let duck = "Duck";
-    let airlines = "Airlines";
-    // let airline_names = [duck, "", airlines].concat();
-    let airline_names = format!("{} {} {}", duck, "", airlines);
-    println!("{}", airline_names);
+    let number: u8 = 65;
+    let letter: char = number as char;
+    println!("{}", letter);
 
-    let mut slogan = String::new();
-    slogan.push_str("We hit the ground");
-    slogan.push(' ');
-    slogan = slogan + "running";
-    println!("{}", slogan);
+    let mut _changeable_varibale = 5;
+
+    let scope_test = "outer scope";
+    println!("scope_test: {}", scope_test);
+    {
+        let scope_test = "inner scope";
+        println!("scope_test: {}", scope_test);
+    }
+    println!("scope_test: {}", scope_test);
 }
